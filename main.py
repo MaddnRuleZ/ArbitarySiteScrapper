9e41e98efbaab01b997205be156b6b1de0f31f11
MaddnRuleZ <106453966+MaddnRuleZ@users.noreply.github.com>
2024-03-31 19:56:32 +0200

    Final Changes, no Changes to RestaurantFile

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import Utilities
+import threading
+
 from ArbitaryScrapper import ArbitaryScrapper
 from ExcelUtils import ExcelUtils
 from MSSQLDatabase import MSSQLDatabase
@@ -16,18 +18,17 @@ def testing_Excel_Stuff():
         print("late")
 
 
-def main():
-    scrapp_eggers()
 
-def scrapp_eggers():
+def scrapp_eggers_thread(url_list, start_index, end_index):
     print("Starting Scrapping Process")
     msql = MSSQLDatabase()
     xlsx = ExcelUtils("dox/RestaurantBarMk1.xlsx", "Sheet1")
 
-    url_list = msql.read_excel_column_by_index("dox/RestaurantBarMk1.xlsx", "Sheet1", 6)
-    print("Read Document fully")
+    for index, url in enumerate(url_list[start_index:end_index], start=start_index+1):
+        # bruh oops
+        if index < 600:
+            continue
 
-    for index, url in enumerate(url_list, start=2):
         url_string = str(url)
         print("Scrapping url :" + url_string)
 
@@ -40,15 +41,37 @@ def scrapp_eggers():
             result = arbitary.get_all_matching_links()
             print(f"---- RESULT for URL {index} ------")
             print(result)
-            if result is None:
+            if result is None or result == "":
                 continue
 
             xlsx.change_cell_content("N", index, result)
-            print("inserted Entry")
+            print("Inserted Entry")
             print("----------------------")
 
         except Exception as e:
             print(f"FATAL ERROR IN URL {index}: {str(e)}")
+
+def start_multi_threaded_scrapping(url_list, num_threads=4):
+    total_urls = len(url_list)
+    chunk_size = total_urls // num_threads
+
+    threads = []
+    for i in range(num_threads):
+        start_index = i * chunk_size
+        end_index = (i + 1) * chunk_size if i < num_threads - 1 else total_urls
+        thread = threading.Thread(target=scrapp_eggers_thread, args=(url_list, start_index, end_index))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+def main():
+    msql = MSSQLDatabase()
+    url_list = msql.read_excel_column_by_index("dox/RestaurantBarMk1.xlsx", "Sheet1", 6)
+
+    print("Read Document fully")
+    start_multi_threaded_scrapping(url_list, num_threads=4)
 
 
 def production():
@@ -73,20 +96,8 @@ def production():
         except:
             print("FATAL ERROR IN URL")
 
-main()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
